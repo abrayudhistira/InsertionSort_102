1bd47794a38dcc1e64421666569a5da8116ecce0
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-03-23 15:14:43 +0700

    membuat inputan jumlah elemen array

--- InsertionSort_102/InsertionSort_102.cpp
+++ InsertionSort_102/InsertionSort_102.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 int arr[20]; //membuat array dengan panjang data 20
 int n;
+
+void input()
+{   //Procedure input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; //membuat inputan jumlah elemen array
